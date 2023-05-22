2c037f9c45faccbf484117cd90a72dfecbbf502b
JU4Nkpl <130647266+JU4Nkpl@users.noreply.github.com>
2023-05-22 08:52:54 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,36 +6,29 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         if (Mi_jugada == 1) {
             basic.showString("empate")
-            basic.clearScreen()
         } else if (Mi_jugada == 2) {
             basic.showString("ganas")
-            basic.clearScreen()
         } else if (Mi_jugada == 3) {
             basic.showString("pierdes")
-            basic.clearScreen()
         }
     } else if (receivedNumber == 2) {
         if (Mi_jugada == 1) {
             basic.showString("pierdes")
-            basic.clearScreen()
         } else if (Mi_jugada == 2) {
             basic.showString("empate")
-            basic.clearScreen()
         } else if (Mi_jugada == 3) {
             basic.showString("ganas")
-            basic.clearScreen()
         }
     } else if (receivedNumber == 3) {
         if (Mi_jugada == 1) {
             basic.showString("ganas")
-            basic.clearScreen()
         } else if (Mi_jugada == 2) {
             basic.showString("pierdes")
-            basic.clearScreen()
         } else if (Mi_jugada == 3) {
             basic.showString("empate")
-            basic.clearScreen()
         }
+        basic.pause(2000)
+        basic.clearScreen()
     }
 })
 input.onButtonPressed(Button.A, function () {
