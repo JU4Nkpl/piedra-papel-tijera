4e8daf42c53336fba9aae9f9af6f5f59f3887d52
JU4Nkpl <130647266+JU4Nkpl@users.noreply.github.com>
2023-05-22 08:38:05 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 let Mi_jugada = 0
 let piedra = 0
-let papel = 0
 let tijera = 0
+let papel = 0
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         if (Mi_jugada == 1) {
@@ -38,50 +38,48 @@ radio.onReceivedNumber(function (receivedNumber) {
         }
     }
 })
+input.onButtonPressed(Button.A, function () {
+    radio.setGroup(220)
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    piedra = 1
+    Mi_jugada = 1
+    radio.sendNumber(1)
+})
 input.onGesture(Gesture.Shake, function () {
     basic.clearScreen()
 })
-basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        radio.setGroup(220)
-        basic.pause(500)
-        basic.showLeds(`
-            . # # # .
-            # # # # #
-            # # # # #
-            # # # # #
-            . # # # .
-            `)
-        piedra = 1
-        Mi_jugada = 1
-        radio.sendNumber(1)
-    }
-    if (input.buttonIsPressed(Button.B)) {
-        radio.setGroup(220)
-        basic.pause(500)
-        basic.showLeds(`
-            . # # # .
-            . # # # .
-            . # # # .
-            . # # # .
-            . . . . .
-            `)
-        papel = 2
-        Mi_jugada = 2
-        radio.sendNumber(2)
-    }
-    if (input.buttonIsPressed(Button.AB)) {
-        radio.setGroup(220)
-        basic.pause(500)
-        basic.showLeds(`
-            # # . # #
-            # # . # #
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
-        tijera = 3
-        Mi_jugada = 3
-        radio.sendNumber(3)
-    }
+input.onButtonPressed(Button.AB, function () {
+    radio.setGroup(220)
+    basic.pause(500)
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    tijera = 3
+    Mi_jugada = 3
+    radio.sendNumber(3)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.setGroup(220)
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    papel = 2
+    Mi_jugada = 2
+    radio.sendNumber(2)
 })
