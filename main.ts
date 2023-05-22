9c345a4a36b463af6ce7eb9173fa673a08450841
JU4Nkpl <130647266+JU4Nkpl@users.noreply.github.com>
2023-05-22 08:31:16 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,40 @@
+let Mi_jugada = 0
 let piedra = 0
 let papel = 0
 let tijera = 0
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        if (true) {
-        	
+        if (Mi_jugada == 1) {
+            basic.showString("empate")
+            basic.clearScreen()
+        } else if (Mi_jugada == 2) {
+            basic.showString("ganas")
+            basic.clearScreen()
+        } else if (Mi_jugada == 3) {
+            basic.showString("pierdes")
+            basic.clearScreen()
         }
     } else if (receivedNumber == 2) {
-        if (true) {
-        	
+        if (Mi_jugada == 1) {
+            basic.showString("pierdes")
+            basic.clearScreen()
+        } else if (Mi_jugada == 2) {
+            basic.showString("empate")
+            basic.clearScreen()
+        } else if (Mi_jugada == 3) {
+            basic.showString("ganas")
+            basic.clearScreen()
         }
     } else if (receivedNumber == 3) {
-        if (true) {
-        	
+        if (Mi_jugada == 1) {
+            basic.showString("ganas")
+            basic.clearScreen()
+        } else if (Mi_jugada == 2) {
+            basic.showString("pierdes")
+            basic.clearScreen()
+        } else if (Mi_jugada == 3) {
+            basic.showString("empate")
+            basic.clearScreen()
         }
     }
 })
@@ -28,6 +50,7 @@ basic.forever(function () {
             . # # # .
             `)
         piedra = 1
+        Mi_jugada = 1
         radio.sendNumber(1)
     }
     if (input.buttonIsPressed(Button.B)) {
@@ -41,6 +64,7 @@ basic.forever(function () {
             . . . . .
             `)
         papel = 2
+        Mi_jugada = 2
         radio.sendNumber(2)
     }
     if (input.buttonIsPressed(Button.AB)) {
@@ -54,6 +78,7 @@ basic.forever(function () {
             # . . . #
             `)
         tijera = 3
+        Mi_jugada = 3
         radio.sendNumber(3)
     }
 })
